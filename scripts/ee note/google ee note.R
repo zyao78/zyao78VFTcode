dbd0f8fe076885856b81f4b6f90525bd327c371e
Zheng Yao <zyao78@ksu.edu>
2025-08-26 18:14:49 -0500

    8/26

---
+++ scripts/ee note/google ee note.R
@@ -0,0 +1,8 @@
+## change directory
+cd /d F:\VFT\VFT_github\zyao78VFTcode
+
+#### check ee api version in Anaconda prompt
+conda activate vft
+python -c "import ee, sys; print('ee:', ee.__version__); print(sys.executable)" 
+
+
