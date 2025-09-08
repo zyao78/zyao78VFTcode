063f4087bcb638315619480644dfc0305aadf262
Zheng Yao <zyao78@ksu.edu>
2025-09-08 17:14:14 -0500

    9/8

--- scripts/14_resids.R
+++ scripts/14_resids.R
@@ -3,15 +3,11 @@ library(DHARMa)
 
 par(mfrow = c(1, 1))
 # simulate residuals  #https://cran.r-project.org/web/packages/DHARMa/vignettes/DHARMa.html extended reading on DHARMa
-residuals_sur_g <- simulateResiduals(fittedModel = sur, plot = F)
-plotResiduals(residuals_sur_g, quantreg = TRUE)
+residuals <- simulateResiduals(fittedModel = crep_mod_R, plot = F)
+#plotQQunif(residuals_sur) ### KS test (test for correct distribution), dispersion test, outlier test
+#plotResiduals(residuals_sur, quantreg = TRUE)
 
-plotResiduals(residuals_sur_g) 
-
-
-
-residuals_vargrowth <- simulateResiduals(fittedModel=vargrowth_mod)   
-plotResiduals(residuals_vargrowth)
+##  plotResiduals(residuals_sur, form = sur_subset_R$site)   plot residual against one of the model variable
+plot(residuals)  # plot qq plot and simulated residual plot
 
 
-plot(vargrowth_mod)
