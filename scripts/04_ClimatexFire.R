a01b9ce8fd37c2c5512182fd085d9cc24f2221b5
Zheng Yao <zyao78@ksu.edu>
2025-10-23 18:21:10 -0500

    10/23

--- scripts/04_ClimatexFire.R
+++ scripts/04_ClimatexFire.R
@@ -7,118 +7,101 @@ install.packages("tidyverse")
 install.packages("MuMIn")
 install.packages("glue")
 install.packages("dplyr")
-
-
 library(AICcmodavg)
 library(lme4)
 library(tidyverse)
 library(MuMIn)
 library(glue)
-HOBO_upto2024$site[HOBO_upto2024$site == "GSP_BI"] <- "GSP-BI"
-HOBO_upto2024$site[HOBO_upto2024$site == "GSP_LI"] <- "GSP-LI"
-SoilT_upto2024$site[SoilT_upto2024$site == "GSP_BI"] <- "GSP-BI"
-SoilT_upto2024$site[SoilT_upto2024$site == "GSP_LI"] <- "GSP-LI"
 
-###### local Precipitation variables
-monthlySM <- HOBO_upto2024 %>%
-  group_by(newMonth, startyear, site) %>%    # for startyear of 2021 for example, months 6-12 were in 2021 and 1-5 were in 2015.
-  summarize(meanSM = mean(mSWC))
-monthlyST <- SoilT_upto2024 %>%   ## fix NAs, check 2000 startyear???
-  group_by(newMonth, startyear, site) %>%    # for startyear of 2021 for example, months 6-12 were in 2021 and 1-5 were in 2015.
-  summarize(meanST = mean(Value,na.rm = TRUE))
-MonthlyCV <- full_join(monthlySM, monthlyST, by = c("startyear", "site", "newMonth"))   # use full_join to not cut rows
+HOBO <- read_csv("F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/HOBO_upto2024.csv")
+SoilT <- read_csv("F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/SoilT_upto2024.csv")
+
+table(HOBO$site)
+
+HOBO$site[HOBO$site == "GSP_BI"] <- "GSP-BI"
+HOBO$site[HOBO$site == "GSP_LI"] <- "GSP-LI"
+SoilT$site[SoilT$site == "GSP_BI"] <- "GSP-BI"
+SoilT$site[SoilT$site == "GSP_LI"] <- "GSP-LI"
 
 
-## annual mean precipitation (potential issue with sample size imbalance)
+#### some high values of soilT in CM are in F, fix those
+T_inF <- SoilT[which(SoilT$Value >=38 ),]    
+Distinct_time <- T_inF %>%
+  distinct(site, month, year) %>%
+  arrange(year, site, month)   ### problematic entires: CM: 6/20/18 to 6/17/19, 6/16/19 to 6/14/20
+                               #                        CH: 6/16/19 to 6/14/20
 
-P_LA<- HOBO_upto2024 %>%
+cm_start <- mdy("6/19/2018")+ days(1) - seconds(1)
+cm_end   <- mdy("6/14/2020") + days(1) - seconds(1)
+ch_start  <- mdy("6/15/2019")+ days(1) - seconds(1)
+ch_end    <- mdy("6/14/2020") + days(1) - seconds(1)
+
+T_inF <- SoilT %>%
+  filter(
+    (Value >=38&site == "CM" & date_parsed >= cm_start & date_parsed <= cm_end) |
+  (Value >=38&site == "CH" & date_parsed >= ch_start  & date_parsed <= ch_end)
+  )
+T_inF <- T_inF %>%
+  filter(!(Value <=50 & month>=4 & month<=9))
+T_inF$Value_C <- (T_inF$Value - 32) * 5/9
+
+for (i in 1:nrow(SoilT)) {
+  match_value <- T_inF$Value_C[SoilT$Date.Time [i] == T_inF$Date.Time & SoilT$site [i] == T_inF$site]
+  if (length(match_value) == 1) {
+    SoilT$Value[i] <- match_value
+  }
+}   ### this might take a long time to run (>2 min)
+hist(SoilT$Value)   ## all fixed
+
+write.csv(SoilT, "F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/SoilT_upto2024.csv")
+
+
+
+######           local climate variables
+#####
+#####
+#####
+Climvar<- read.csv("data/TBFxClimate/Climvar_combined_interpolated.csv")
+colnames(Climvar)
+TBF_long<- read.csv("data/TBFxClimate/TBF_long.csv")
+colnames(TBF_long)
+
+## summarise by startyear
+TP_LA<- Climvar %>%
   group_by(startyear, site) %>%    # for startyear of 2021 for example, months 6-12 were in 2021 and 1-5 were in 2022.
-  summarize(mean = mean(mSWC,na.rm = TRUE))
+  summarise(T_LA = mean(local_T,na.rm = TRUE),P_LA = mean(local_P,na.rm = TRUE))
+## summarise by newMonth (from 15th to 14th_month2)
+TP_LM<- Climvar %>%
+  group_by(startyear, site, newMonth) %>%    
+  summarise(T_LM = mean(local_T,na.rm = TRUE),P_LM = mean(local_P,na.rm = TRUE))
 
-TBF_long$P_LA <- NA
-
-for (i in 1:nrow(TBF_long)) {
-  match <- P_LA$mean[P_LA$site == TBF_long$site[i] & P_LA$startyear == TBF_long$startyear[i]]
-  
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$P_LA[i] <- match
-    
-  }
-}
-summary(aov(mean ~ site, P_LA))
-ggplot(P_LA, aes(x = site, y = mean, fill = factor(startyear))) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme_minimal() +
-  labs(x = "Site", y = "Mean", fill = "Start Year")+
-  ggtitle("Annual mean local precipitation")
-
-## wettest month SM
-
-P_LW <- monthlySM [monthlySM$newMonth == 8, ]
-TBF_long$P_LW <- NA
-for (i in 1:nrow(TBF_long)) {
-  match <- P_LW$meanSM[P_LW$site == TBF_long$site[i] & P_LW$startyear == TBF_long$startyear[i]]
-  
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$P_LW[i] <- match
-    
-  }
-}
-
-## driest month  
-
-P_LD <- monthlySM [monthlySM$newMonth == 12, ]  
-
-TBF_long$P_LD <- NA
-
-for (i in 1:nrow(TBF_long)) {
-  match <- P_LD$meanSM[P_LD$site == TBF_long$site[i] & P_LD$startyear == TBF_long$startyear[i]]
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$P_LD[i] <- match
-  }
-}
-
-## hottest month  (big issue with gaps in data availability) fix later
-
-P_LH <- monthlySM [monthlySM$newMonth == 7, ]  
-
-TBF_long$P_LH <- NA
-
-for (i in 1:nrow(TBF_long)) {
-  match <- P_LH$meanSM[P_LH$site == TBF_long$site[i] & P_LH$startyear == TBF_long$startyear[i]]
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$P_LH[i] <- match
-  }
-}
-
-
-
-
-###### local temperature variables
 ###
 ### ## annual mean (potential issue with sample size imbalance)
 
-T_LA<- SoilT_upto2024 %>%
-  group_by(startyear, site) %>%    # for startyear of 2021 for example, months 6-12 were in 2021 and 1-5 were in 2022.
-  summarize(mean = mean(Value,na.rm = TRUE))
 
 TBF_long$T_LA <- NA
+TBF_long$P_LA <- NA
+
 
 for (i in 1:nrow(TBF_long)) {
-  match <- T_LA$mean[T_LA$site == TBF_long$site[i] & T_LA$startyear == TBF_long$startyear[i]]
+  match_LT <- TP_LA$T_LA[TP_LA$site == TBF_long$site[i] & TP_LA$startyear == TBF_long$startyear[i]]
+  match_LP <- TP_LA$P_LA[TP_LA$site == TBF_long$site[i] & TP_LA$startyear == TBF_long$startyear[i]]
   
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$T_LA[i] <- match
-    
+  if (length(match_LT) >= 1) {  
+    TBF_long$T_LA[i] <- match_LT}
+  if (length(match_LP) >= 1) {
+    TBF_long$P_LA[i] <- match_LP
   }
 }
+head(TBF_long)
+
 
 ### coldest month min daily T
-
-T_LC <- monthlyST [monthlyST$newMonth == 1,] 
-minST <- SoilT_upto2024 %>%
+###
+T_LC <- TP_LM [TP_LM$newMonth == 1,] 
+minST <- Climvar %>%
   group_by(newMonth, startyear, site) %>%
-  summarize(minST = min(Value, na.rm = TRUE), .groups = "drop")
+  summarize(minST = min(local_T, na.rm = TRUE), .groups = "drop")
 T_LC <- T_LC %>%
   left_join(minST, by = c("newMonth", "startyear", "site"))
 
@@ -130,52 +113,72 @@ for (i in 1:nrow(TBF_long)) {
     TBF_long$T_LC[i] <- match
   }
 }
+head(TBF_long)
 
-### hottest month T 
+### hottest month T and P
 
-T_LH <- monthlyST [monthlyST$newMonth == 7,] 
-maxST <- SoilT_upto2024 %>%
+T_LH <- TP_LM [TP_LM$newMonth == 7,] 
+maxLH <- Climvar %>%
   group_by(newMonth, startyear, site) %>%
-  summarize(maxST = max(Value, na.rm = TRUE), .groups = "drop")
+  summarize(maxST = max(local_T, na.rm = TRUE),
+            maxLP=  max(local_P, na.rm = TRUE),
+            .groups = "drop")  
 T_LH <- T_LH %>%
-  left_join(maxST, by = c("newMonth", "startyear", "site"))
+  left_join(maxLH, by = c("newMonth", "startyear", "site"))
 
 TBF_long$T_LH <- NA
+TBF_long$P_LH <- NA
 
 for (i in 1:nrow(TBF_long)) {
-  match <- T_LH$maxST[T_LH$site == TBF_long$site[i] & T_LH$startyear == TBF_long$startyear[i]]
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$T_LH[i] <- match
+  match_T <- T_LH$maxST[T_LH$site == TBF_long$site[i] & T_LH$startyear == TBF_long$startyear[i]]
+  match_P <- T_LH$maxLP[T_LH$site == TBF_long$site[i] & T_LH$startyear == TBF_long$startyear[i]]
+  
+  if (length(match_T) >= 1) {  # Ensure there's exactly one match
+    TBF_long$T_LH[i] <- match_T}
+  if (length(match_P) >= 1) {
+    TBF_long$P_LH[i] <- match_P
   }
 }
+head(TBF_long)
 
 ### wettest month T
-MonthlyCV$startyear <- as.factor(MonthlyCV$startyear)
-MonthlyCV$site <- as.factor(MonthlyCV$site)
 
-T_LW <- monthlyST [monthlyST$newMonth == 8,] 
+
+Wettest_L <- TP_LM [TP_LM$newMonth == 8,] 
 TBF_long$T_LW <- NA
+TBF_long$P_LW <- NA
 
 for (i in 1:nrow(TBF_long)) {
-  match <- T_LW$meanST[T_LW$site == TBF_long$site[i] & T_LW$startyear == TBF_long$startyear[i]]
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$T_LW[i] <- match
+  match_T <- Wettest_L$T_LM[Wettest_L$site == TBF_long$site[i] & Wettest_L$startyear == TBF_long$startyear[i]]
+  match_P <- Wettest_L$P_LM[Wettest_L$site == TBF_long$site[i] & Wettest_L$startyear == TBF_long$startyear[i]]
+  
+  if (length(match_T) >= 1) {  # Ensure there's exactly one match
+    TBF_long$T_LW[i] <- match_T}
+  if (length(match_P) >= 1) {
+    TBF_long$P_LW[i] <- match_P
   }
 }
 
+head(TBF_long)
 
 ### driest month T
 
-T_LD <- monthlyST [monthlyST$newMonth == 12,] 
+driest_L <- TP_LM [TP_LM$newMonth == 12,] 
 
 TBF_long$T_LD <- NA
+TBF_long$P_LD <- NA
 
 for (i in 1:nrow(TBF_long)) {
-  match <- T_LD$meanST[T_LD$site == TBF_long$site[i] & T_LD$startyear == TBF_long$startyear[i]]
-  if (length(match) >= 1) {  # Ensure there's exactly one match
-    TBF_long$T_LD[i] <- match
+  match_T <- driest_L$T_LM[driest_L$site == TBF_long$site[i] & driest_L$startyear == TBF_long$startyear[i]]
+  match_P <- driest_L$P_LM[driest_L$site == TBF_long$site[i] & driest_L$startyear == TBF_long$startyear[i]]
+  
+  if (length(match_T) >= 1) {  # Ensure there's exactly one match
+    TBF_long$T_LD[i] <- match_T}
+  if (length(match_P) >= 1) {
+    TBF_long$P_LD[i] <- match_P
   }
 }
+head(TBF_long)
 ####
 ###
 ####
@@ -345,15 +348,30 @@ for (i in 1:nrow(TBF_long)) {
 }
 
 
+### 
+write.csv(TBF_long,"F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/TBF_long_10232025.csv" )
+###
+
+
+
+
+
+
 
 
 ###  fill gaps in local climate data with interpolation
 ### ###
 
-LocalP <- read_csv("data/TBFxClimate/HOBO_upto2024.csv")
-LocalT <- read_csv("data/TBFxClimate/SoilT_upto2024.csv")
-HOBO_upto2024 <- read_csv("data/TBFxClimate/HOBO_upto2024.csv")
-RegionalTP<-read_csv("data/TBFxClimate/Reg_clim_upto2024.csv")
+LocalP <- read.csv("data/TBFxClimate/HOBO_upto2024.csv")
+LocalT <- read.csv("data/TBFxClimate/SoilT_upto2024.csv")
+table(LocalP$site)
+table(LocalT$site)
+
+RegionalTP<-read.csv("data/TBFxClimate/Reg_clim_upto2024.csv")
+
+table(RegionalTP$site)
+
+
 
 ### checks for month absence
 arr <- with(LocalT, tapply(month, list(site, year, factor(month, levels = 1:12)), length))
@@ -374,7 +392,7 @@ Combined$local_T <- NA
 Combined$local_P <- NA
 
 
-
+### paste daily Local T and P into the Combined
 for (i in 1:nrow(Combined)) {
   match <- LocalT_daily$meanST[LocalT_daily$site == Combined$site[i] & 
                                  LocalT_daily$startyear == Combined$startyear[i] &
@@ -393,7 +411,6 @@ for (i in 1:nrow(Combined)) {
     Combined$local_P[i] <- match
   }
 }
-any(is.na(Combined$local_P))
 
 Combined <- Combined %>%
   rename(
@@ -401,25 +418,30 @@ Combined <- Combined %>%
     regional_P = prec
   )
 colnames(Combined)
+table(Combined$site)
 
-plot(Combined$regional_T, Combined$local_T)
+plot(Combined$regional_T, Combined$local_T) ### much cleaner correlation after fixing the entries with the wrong unit
 plot(Combined$regional_P, Combined$local_P) ### variables on very different scale, hard to visualize
 
 Combined$s.regional_P <- NA
 Combined$s.local_P  <- NA
 Combined$s.logregional_P <- NA
 
-Combined$c <- scale(log(Combined$regional_P+0.1))
+Combined$s.logregional_P <- scale(log(Combined$regional_P+0.1))
 Combined$s.local_P  <- scale(Combined$local_P)
 hist(Combined$s.logregional_P)
 
+write.csv(Combined, "F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/Climvar_combined.csv")
 
 
 #####            linear model predicting local temperature       #######
 ####
 ####
 ###
-## allow slopes to vary among sites + T*P interaction
+Combined <- read.csv("data/TBFxClimate/Climvar_combined.csv")
+
+
+## simple linear model that allow slopes to vary among sites 
 Local_T_subset <- 
   Combined %>% 
   filter_at(vars(local_T,regional_T,s.logregional_P,site), all_vars(!is.na(.)))
@@ -427,96 +449,113 @@ Local_T_subset <-
 GM_localT <- lm(local_T~regional_T+
                    site+site:regional_T,data=Local_T_subset,
                 na.action = "na.fail")    
+summary(GM_localT)
+hist(GM_localT$residuals) ## great model fit
 
-dredge_localT <- MuMIn::dredge(
-  GM_localT,
-  trace   = 2
-)
-lm_localT <- get.models(dredge_localT, 1)[[1]]
-  ## local P
+
+#### local P
 Local_P_subset <- 
   Combined %>% 
   filter_at(vars(local_P,regional_T,s.logregional_P,site), all_vars(!is.na(.)))
 
-GM_localP <- lm(local_P~regional_T+s.logregional_P+
-                  site+regional_T:s.logregional_P+site:s.logregional_P,data=Local_P_subset,
+
+
+GM_localP <- lm(local_P~s.logregional_P+
+                  site+site:s.logregional_P ,data=Local_P_subset,
                 na.action = "na.fail")    
-dredge_localP <- MuMIn::dredge(
-  GM_localP,
-  trace   = 2
-)
-lm_localP <- get.models(dredge_localP, 1)[[1]]
+hist(Local_P_subset$local_P)
+summary(GM_localP)
+hist(GM_localP$residuals) ## worse R-squared, but not horrible
+
+### trying GAM sine the zero-inflated predictor is lowering the predictive power of the model
+## refer to Zak's GAM codes
 
 ##check residuals
 par(mfrow = c(2, 2))
-summary(lm_localT)
-plot(lm_localP)
-plot(lm_localT)
+summary(GM_localP)
+plot(GM_localP)
 
-### local_T model looks funky,
+###
+###
+####                      Gap filling for local T and P
 
-## log local_T
-Combined$logLocal_T <- NA
-Combined$logLocal_T <- log(Combined$local_T)
-hist(Combined$logLocal_T)   ## slightly more normal try building a model
-Local_T_log_subset <- 
-  Combined %>% 
-  filter_at(vars(logLocal_T,regional_T,s.logregional_P,site), all_vars(!is.na(.)))
-GM_localT_log <- lm(logLocal_T~regional_T+
-                       site+site:regional_T,data=Local_T_log_subset,
-                     na.action = "na.fail")    
-dredge_localT_log <- MuMIn::dredge(
-  GM_localT_log,
-  trace   = 2
-)
-lm_localT_log <- get.models(dredge_localT_log, 1)[[1]]
-summary(lm_localT_log)
-plot(lm_localT_log)
-hist(lm_localT_log$residuals)  ### model fit has improved substantially, keep localT_log, don't proceed further
+gap_in_LT <- Combined[which(is.na(Combined$local_T)),c("local_T", "regional_T", "site", "X")]
+gap_in_LP <- Combined[which(is.na(Combined$local_P)),c("local_P", "s.logregional_P", "site", "X")]
 
-#### some high unusual soil T are in F, convert those (32°F − 32) × 5/9 = 0°C
+gap_in_LT$local_T<-predict(GM_localT, gap_in_LT, interval = "prediction")[,1]
+gap_in_LP$local_P<-predict(GM_localP, gap_in_LP, interval = "prediction")[,1]### the first column is fitted value
 
 
-
-### try removing the possible fire induced soilT peaks, this is not recommended
-hist(Combined$local_T)
-Combined$local_T_rm_ex <- NA
-Combined$local_T_rm_ex <- ifelse(Combined$local_T < 40, Combined$local_T, NA )
-Local_T_rmex_subset <- 
-  Combined %>% 
-  filter_at(vars(local_T_rm_ex,regional_T,s.logregional_P,site), all_vars(!is.na(.)))
-GM_localT_rmex <- lm(local_T_rm_ex~regional_T+s.logregional_P+
-                  site+regional_T:s.logregional_P+site:regional_T,data=Local_T_rmex_subset,
-                na.action = "na.fail")    
-dredge_localT_rmex <- MuMIn::dredge(
-  GM_localT_rmex,
-  trace   = 2
-)
-lm_localT_rmex <- get.models(dredge_localT_rmex, 1)[[1]]
-summary(lm_localT_rmex)
-par(mfrow = c(2, 2))
-plot(lm_localT_rmex)    
+for (i in 1:nrow(Combined)) { 
+  if(is.na(Combined$local_T[i])){
+ match_row <- gap_in_LT[which(Combined$X[i] == gap_in_LT$X),]
+ if(nrow(match_row) == 1){
+   Combined$local_T[i] <- match_row$local_T
+     }
+  }}
+for (i in 1:nrow(Combined)) { 
+  if(is.na(Combined$local_P[i])){
+    match_row <- gap_in_LP[which(Combined$X[i] == gap_in_LP$X),]
+    if(nrow(match_row) == 1){
+      Combined$local_P[i] <- match_row$local_P
+    }
+  }}
 
 
+write.csv(Combined, "F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/Climvar_combined_interpolated.csv")
+
+#### 
+#### since daily local_P is zero inflated, the following codes try to model monthly local_P to see if that improves 
+####  the model fit
+
+Combined_2 <- read.csv("data/TBFxClimate/Climvar_combined.csv")
+
+Monthly_Local_P <- Combined_2 %>% 
+  group_by(site, newMonth,startyear) %>%
+  dplyr::summarise(local_P_M = mean(local_P, na.rm = TRUE), regional_P_M = sum(regional_P))
+
+Monthly_Local_P$local_P_M[which(is.nan(Monthly_Local_P$local_P_M))] <- NA
+Monthly_Local_P$s.logregional_P_M <- scale(log(Monthly_Local_P$regional_P_M))
 
 
+summary(lm(local_P_M~s.logregional_P_M+
+             site+site:s.logregional_P_M,data=Monthly_Local_P,
+           na.action = "na.omit"))
 
 
+summary(lm(local_P_M ~ regional_P_M, data=Monthly_Local_P))
+summary(lm(local_P ~ s.logregional_P, data=Combined_2))
 
-##### checking large scale climate trend
-Monthlyclimsum <- RegionalTP %>%
-  group_by(newMonth,site) %>%
-  summarize(meanP = mean (prec), meanT = mean(Temp, na.rm = T))
-
-Monthlyclimsum$newMonth <- as.factor()
-
-ggplot(data= Monthlyclimsum, aes(x= newMonth, y= meanT, group = site, color = site)) +
-  geom_line(linewidth = 1) + 
-  #geom_ribbon(aes(ymin = data_new1$sur$lwr, ymax = data_new1$sur$upr, fill= newTBF), alpha = 0.1) + 
-  labs(y= "mean monthly temperature (2015-2024)", x = "Month (M/15 - M+1/14)") +
-  #theme_bw() + theme(legend.position='none') +
-  theme(legend.position = "right") +
-  theme(text = element_text(size = 10))
+plot(Monthly_Local_P$local_P_M ~ Monthly_Local_P$s.logregional_P_M)
+plot(Combined_2$local_P ~ Combined_2$s.logregional_P)
 
 
-write.csv(Combined, "F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/Climvar_combined.csv")
+#### plotting some large scale climate trend
+
+monthly_LP <- Climvar %>%
+  group_by(newMonth, site) %>%
+  summarise(meanSM = mean(local_P, na.rm = TRUE), .groups = "drop") #, .groups = "drop"
+Monthly_Local_P_averaged <- Monthly_Local_P %>%
+  group_by(site, newMonth) %>%
+  summarise(local_P_M = mean(local_P_M, na.rm = TRUE), .groups = "drop") %>%
+  arrange(site, newMonth)
+
+ggplot(Monthly_Local_P_averaged,
+       aes(x = factor(newMonth), y = local_P_M, color = site, group = site)) +
+  geom_line(size = 1,na.rm = TRUE) +
+  geom_point(size = 1.6, na.rm = TRUE) +
+  labs (x= "month", y="mean soil moisture")
+
+
+B1_climate <- Climvar[which(Climvar$site == "B1"),]
+B1_climate_monthly <- B1_climate %>%
+  group_by(newMonth, startyear) %>%
+  summarise(B1_climate_monthly = mean(local_P, na.rm=T), .groups = "drop")
+
+ggplot(B1_climate_monthly,
+       aes(x = factor(newMonth), y = B1_climate_monthly)) +
+  geom_line(size = 1) +
+  geom_point(size = 1.6, na.rm = TRUE) +
+  labs (x= "month", y="mean soil moisture")
+
+  
--- scripts/21_ClimatexFire_modeling.R
+++ scripts/21_ClimatexFire_modeling.R
@@ -60,12 +60,12 @@ TBF_long$s.sq.TSF <- scale(TBF_long$sq.TSF)
 TBF_long$s.TBF <- scale(TBF_long$TBF)
 
 ##
-TBF_long <- read_csv("data/TBFxClimate/TBF_long.csv") 
+TBF_long <- read.csv("data/TBFxClimate/TBF_long_10232025.csv") 
 colnames(TBF_long)
 
 
 ###
-###                 Regional  climate variables (Annual + Monthly)
+###                Regional   climate variables (Annual + Monthly)
 ###
 ###         Define global mode according to Louthan et.al., 2022, Table 2.
 ###
@@ -311,10 +311,288 @@ summary(prep_mod_R)
 
 stopCluster(cluster)
 
+###################################################################################################################
+###################################################################################################################
+###################################################################################################################
+###################################################################################################################
+### 
+###                       Local   climate variables (Annual + Monthly)
+###
+###               Define global mode according to Louthan et.al., 2022, Table 2.
+###
+###
+###
+###
+TBF_long$s.T_LA <- NA
+TBF_long$s.T_LA <- scale(TBF_long$T_LA) 
+TBF_long$s.P_LA <- NA
+TBF_long$s.P_LA <- scale(TBF_long$P_LA) 
+TBF_long$s.P_LW <- NA
+TBF_long$s.P_LW <- scale(TBF_long$P_LW) 
+TBF_long$s.P_LD <- NA
+TBF_long$s.P_LD <- scale(TBF_long$P_LD) 
+TBF_long$s.P_LH <- NA
+TBF_long$s.P_LH <- scale(TBF_long$P_LH) 
+TBF_long$s.T_LH<- NA
+TBF_long$s.T_LD<- NA
+TBF_long$s.T_LW<- NA
+TBF_long$s.T_LC<- NA
+TBF_long$s.T_LH <- scale(TBF_long$T_LH) 
+TBF_long$s.T_LC <- scale(TBF_long$T_LC) 
+TBF_long$s.T_LD <- scale(TBF_long$T_LD) 
+TBF_long$s.T_LW <- scale(TBF_long$T_LW) 
+
+TBF_long$sq.P_LA <- (TBF_long$P_LA)^2
+TBF_long$sq.T_LA <- (TBF_long$T_LA)^2
+TBF_long$sq.T_LH <- (TBF_long$T_LH)^2
+TBF_long$s.sq.T_LH <- scale(TBF_long$sq.T_LH)
+TBF_long$s.sq.P_LA <- scale(TBF_long$sq.P_LA)
+TBF_long$s.sq.T_LA <- scale(TBF_long$sq.T_LA)
+TBF_long$sq.TSF <- (TBF_long$TSF)^2
+TBF_long$s.TSF <- scale(TBF_long$TSF)
+TBF_long$s.sq.TSF <- scale(TBF_long$sq.TSF)
+TBF_long$s.TBF <- scale(TBF_long$TBF)
+
+
+### Survival
+sur_subset_L <- 
+  TBF_long %>% 
+  filter_at(vars(consur0_1, s.logsize0, s.TSF,s.TBF, s.sq.TSF, site, s.T_LA,s.T_LH,s.T_LD,s.sq.T_LH,
+                 s.P_LH, s.P_LD), all_vars(!is.na(.)))
+
+GM_L_sur <- glmer(
+  consur0_1 ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF+
+    s.T_LA + s.T_LH + s.sq.T_LH + s.T_LD +s.P_LH +
+    s.P_LH:s.T_LH + s.P_LD + 
+    (1 | site),
+  data = sur_subset_L,
+  family = "binomial", 
+  na.action = "na.fail"
+)
+
+## treating site as fixed effect
+GM_L_sur_2 <- glm(
+  consur0_1 ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF+
+    s.T_LA + s.T_LH + s.sq.T_LH + s.T_LD +s.P_LH +
+    s.P_LH:s.T_LH + s.P_LD + 
+    site,
+  data = sur_subset_L,
+  family = "binomial", 
+  na.action = "na.fail"
+)
+
+summary(GM_L_sur_2)
+
+
+n_cores <- detectCores()
+cluster <- makeCluster(n_cores - 1)
+registerDoParallel(cluster)
+clusterExport(cluster, c("sur_subset_L"))   ### replace with different subset (different global models)
+clusterEvalQ(cluster, {library(lme4); library(MuMIn)})
+
+sur_dredge_L_2 <- MuMIn::dredge(
+  GM_L_sur_2,
+  cluster = cluster,
+  trace   = 2
+)
+sur_mod_L_2 <- get.models(sur_dredge_L_2, 1)[[1]]
+summary(sur_mod_L_2)   
+
+dfs <- sur_subset_R
+dfs[,17:ncol(dfs)] <- scale(dfs[,17:ncol(dfs)]) ## scale num variables (not including consur_0_1)
+sur_mod_R_s <- update(sur_mod_R,data=dfs)
+summary(sur_mod_R_s)
+
+cc <- check_collinearity(sur_mod_R)
+cc
+
+
+stopCluster(cluster)
+
+### growth
+
+gr_subset_L_2 <- 
+  TBF_long[which(TBF_long$consur0_1 == 1),] %>%      ## make sure to subset data to only alive plants
+  filter_at(vars(logsize1, s.logsize0, s.TSF,s.sq.TSF, s.TBF, site, s.sq.T_LA,s.T_LA, s.P_LA, s.T_LC, 
+                 s.T_LD, s.P_LH), all_vars(!is.na(.)))
+GM_L_gr <- lmer(
+  logsize1 ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF + s.T_LA + s.sq.T_LA + s.P_LA + s.T_LA:s.P_LA +
+    s.T_LC + s.T_LD + s.P_LH +
+    (1 | site),
+  data = gr_subset_L_2,
+  na.action = "na.fail"
+)
+
+n_cores <- detectCores()
+n_cores
+cluster <- makeCluster(n_cores - 1)
+registerDoParallel(cluster)
+clusterExport(cluster, c("gr_subset_L_2"))   ### replace with different subset (different global models)
+clusterEvalQ(cluster, {library(lme4); library(MuMIn)})
+
+gr_dredge_L <- MuMIn::dredge(
+  GM_L_gr,
+  cluster = cluster,
+  trace   = 2
+)
+gr_mod_L <- get.models(gr_dredge_L, 1)[[1]]
+summary(gr_mod_L)
+car::Anova(gr_mod_L, type = 3)
+stopCluster(cluster)
+
+## prob fruiting
+
+prep_subset_L <- 
+  TBF_long %>% 
+  filter_at(vars(prep1, s.logsize0, s.TSF, s.TBF,s.sq.TSF, site, s.T_LA,s.sq.T_LA,s.sq.P_LA,
+                 s.P_LA, s.T_LH, s.T_LC, 
+                 s.P_LH, ), all_vars(!is.na(.)))
+
+GM_L_prep <- glmer(
+  prep1 ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF+
+    s.T_LA + s.sq.T_LA +s.P_LA+ s.sq.P_LA + s.T_LA:s.P_LA+
+    s.T_LH + s.P_LH + s.T_LH:s.P_LH + s.T_LC +
+    (1 | site),
+  data = prep_subset_L,
+  family = "binomial", 
+  na.action = "na.fail"
+)
+
+n_cores <- detectCores()
+n_cores
+cluster <- makeCluster(n_cores - 1)
+registerDoParallel(cluster)
+clusterExport(cluster, c("prep_subset_L"))   ### replace with different subset (different global models)
+clusterEvalQ(cluster, {library(lme4); library(MuMIn)})
+
+prep_dredge_L <- MuMIn::dredge(
+  GM_L_prep,
+  cluster = cluster,
+  trace   = 2
+)
+prep_mod_L <- get.models(prep_dredge_L, 1)[[1]]
+
+## the best fit model doesn't converge, try simple linear model
+
+
+stopCluster(cluster)
+
+### number of fruit
+
+crep_subset_R <- 
+  TBF_long %>% 
+  filter_at(vars(logcrep1, s.logsize0, TSF, TBF, site, s.T_RA, s.T_RH), all_vars(!is.na(.)))
+
+GM_R_crep <- lmer(
+  logcrep1 ~ s.logsize0 + TSF * TBF + I(TSF^2)+ s.T_RA + s.T_RH +
+    (1 | site),
+  data = crep_subset_R,
+  na.action = "na.fail"
+)
+
+n_cores <- detectCores()
+n_cores
+cluster <- makeCluster(n_cores - 1)
+registerDoParallel(cluster)
+clusterExport(cluster, c("crep_subset_R"))   ### replace with different subset (different global models)
+clusterEvalQ(cluster, {library(lme4); library(MuMIn)})
+
+crep_dredge_R <- MuMIn::dredge(
+  GM_R_crep,
+  cluster = cluster,
+  trace   = 2
+)
+crep_mod_R <- get.models(crep_dredge_R, 1)[[1]]
+summary(crep_mod_R)
+car::Anova(crep_mod_R, type = 3)
+stopCluster(cluster)
+
+## 
+### integrated measure of fruiting 
+
+TBF_long$prep_Int <- NA
+TBF_long$prep_Int <- ifelse(
+  TBF_long$prep1 == 0 & is.na(TBF_long$logcrep1),
+  0,
+  TBF_long$prep1 * TBF_long$crep1
+)
+hist(TBF_long$prep_Int)   ## zero inflated distribution
+
+prep_Int_subset_R <- 
+  TBF_long %>% 
+  filter_at(vars(prep_Int, s.logsize0, s.TSF, s.TBF,s.sq.TSF, site, s.T_RA,s.P_RA, s.T_RH, s.T_RC, 
+                 s.P_RH, s.sq.T_RA), all_vars(!is.na(.)))
+
+GM_R_prep_Int <- glmer.nb(
+  prep_Int ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF+
+    s.T_RA + s.sq.T_RA +s.P_RA + s.T_RA:s.P_RA+
+    s.T_RH + s.P_RH + s.T_RH:s.P_RH + s.T_RC +
+    (1 | site),
+  data = prep_Int_subset_R,
+  na.action = "na.fail",
+  control = glmerControl(optimizer = "bobyqa",
+                         optCtrl   = list(maxfun = 2e5))
+) 
+
+
+
+### attempt to square then scale
+
+TBF_long$sq.P_RA <- (TBF_long$P_RA)^2
+TBF_long$sq.T_RA <- (TBF_long$T_RA)^2
+TBF_long$sq.T_RH <- (TBF_long$T_RH)^2
+TBF_long$s.sq.T_RH <- scale(TBF_long$sq.T_RH)
+TBF_long$sq.TSF <- (TBF_long$TSF)^2
+TBF_long$s.sq.P_RA <- scale(TBF_long$sq.P_RA)
+TBF_long$s.sq.T_RA <- scale(TBF_long$sq.T_RA)
+TBF_long$s.TSF <- scale(TBF_long$TSF)
+TBF_long$s.sq.TSF <- scale(TBF_long$sq.TSF)
+TBF_long$s.TBF <- scale(TBF_long$TBF)
+
+prep_subset_R <- 
+  TBF_long %>% 
+  filter_at(vars(prep1, s.logsize0, TSF, TBF, site, s.T_RA,s.P_RA, s.T_RH, s.T_RC, 
+                 s.P_RH,s.TSF,s.sq.TSF,s.sq.T_RA,s.sq.P_RA), all_vars(!is.na(.)))
+
+
+GM_R_prep_2 <- glm(
+  prep1 ~ s.logsize0 + s.TSF * s.TBF + s.sq.TSF+
+    s.T_RA + s.sq.T_RA +s.P_RA + s.T_RA:s.P_RA+
+    s.T_RH + s.P_RH + s.T_RH:s.P_RH + s.T_RC +
+    site,
+  data = prep_subset_R,
+  family = "binomial", 
+  na.action = "na.fail"
+)       ##### louthan et al., 2022 table 2 minus the quadratic terms for annual precip (hard to interpret)
+
+
+
+n_cores <- detectCores()
+n_cores
+cluster <- makeCluster(n_cores - 1)
+registerDoParallel(cluster)
+clusterExport(cluster, c("prep_subset_R"))   ### replace with different subset (different global models)
+clusterEvalQ(cluster, {library(lme4); library(MuMIn)})
+
+prep_dredge_R <- MuMIn::dredge(
+  GM_R_prep_2,
+  cluster = cluster,
+  trace   = 2
+)
+
+prep_mod_R <- get.models(prep_dredge_R, 1)[[1]]
+summary(prep_mod_R)   
+
+
+
+stopCluster(cluster)
+
+
+
 
 ## save env
 save(list = ls(), file = "env_snapshot.RData")
-save(prep_mod_R, file = "prep_mod_R.Rdata")
+save(gr_mod_L, file = "F:/VFT/VFT_github/zyao78VFTcode/data/TBFxClimate/gr_mod_L.Rdata")
 save(prep_mod_R_2, file = "prep_mod_R_2.Rdata")
 
 ### plotting 
