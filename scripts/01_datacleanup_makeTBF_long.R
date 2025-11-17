c93b7e26c26e6f17e4b21d704e5203f79658903e
Zheng Yao <zyao78@ksu.edu>
2025-11-17 13:33:12 -0600

    11/17

--- scripts/01_datacleanup_makeTBF_long.R
+++ scripts/01_datacleanup_makeTBF_long.R
@@ -47,41 +47,42 @@ for (i in 1:dim(data)[1]){
 
 ## note that starting in 2025, we enforced the new rule for assigning "dead". The following codes reflect this difference
 
-data15_16 <- cbind.data.frame(data[,c("site","ID", "quad", "size15", "rep15", "libsur15_16", "consur15_16", "size16", "N16", "L16","rep16", "Comm16")],  rep(2015, dim(data)[1]))
-data16_17 <- cbind.data.frame(data[,c("site","ID", "quad", "size16", "rep16", "libsur16_17", "consur16_17", "size17","N17", "L17", "rep17",  "Comm17")], rep(2016, dim(data)[1]))                              
-data17_18 <- cbind.data.frame(data[,c("site","ID", "quad", "size17", "rep17", "libsur17_18", "consur17_18", "size18","N18", "L18", "rep18", "Comm18")], rep(2017, dim(data)[1]))                               
-data18_19 <- cbind.data.frame(data[,c("site","ID", "quad", "size18", "rep18", "libsur18_19", "libsur18_19", "size19","N19", "L19", "rep19", "Comm19")], rep(2018, dim(data)[1]))                               
-data19_20 <- cbind.data.frame(data[,c("site","ID", "quad", "size19", "rep19", "libsur19_20", "libsur19_20", "size20","N20", "L20", "rep20", "Comm20")],rep(2019, dim(data)[1]))                                
-data20_21 <- cbind.data.frame(data[,c("site","ID", "quad", "size20", "rep20", "libsur20_21", "libsur20_21", "size21", "N21", "L21","rep21", "Comm21")],rep(2020, dim(data)[1]))                              
-data21_22 <- cbind.data.frame(data[,c("site","ID", "quad", "size21", "rep21", "libsur21_22","libsur21_22", "size22", "N22", "L22", "rep22", "Comm22")], rep(2021, dim(data)[1]))                                
-data22_23 <- cbind.data.frame(data[,c("site","ID", "quad", "size22", "rep22", "libsur22_23","libsur22_23","size23", "N23", "L23", "rep23", "Comm23")], rep(2022, dim(data)[1]))                                
-data23_24 <- cbind.data.frame(data[,c("site","ID", "quad", "size23", "rep23", "libsur23_24","libsur23_24","size24", "N24", "L24", "rep24", "Comm24")], rep(2023, dim(data)[1]))                                
-data24_25 <- cbind.data.frame(data[,c("site","ID", "quad", "size24", "rep24", "libsur24_25","libsur24_25","size25", "N25", "L25", "rep25", "Comm25")], rep(2024, dim(data)[1]))                                
+data15_16 <- cbind.data.frame(data[,c("site","ID", "quad", "size15", "rep15", "sur_15_16", "sur_15_16_tpry", "size16", "N16", "L16","rep16", "Comm16")],  rep(2015, dim(data)[1]))
+data16_17 <- cbind.data.frame(data[,c("site","ID", "quad", "size16", "rep16", "sur_16_17", "sur_16_17_tpry", "size17","N17", "L17", "rep17",  "Comm17")], rep(2016, dim(data)[1]))                              
+data17_18 <- cbind.data.frame(data[,c("site","ID", "quad", "size17", "rep17", "sur_17_18", "sur_17_18_tpry", "size18","N18", "L18", "rep18", "Comm18")], rep(2017, dim(data)[1]))                               
+data18_19 <- cbind.data.frame(data[,c("site","ID", "quad", "size18", "rep18", "sur_18_19", "sur_18_19_tpry", "size19","N19", "L19", "rep19", "Comm19")], rep(2018, dim(data)[1]))                               
+data19_20 <- cbind.data.frame(data[,c("site","ID", "quad", "size19", "rep19", "sur_19_20", "sur_19_20_tpry", "size20","N20", "L20", "rep20", "Comm20")],rep(2019, dim(data)[1]))                                
+data20_21 <- cbind.data.frame(data[,c("site","ID", "quad", "size20", "rep20", "sur_20_21", "sur_20_21_tpry", "size21", "N21", "L21","rep21", "Comm21")],rep(2020, dim(data)[1]))                              
+data21_22 <- cbind.data.frame(data[,c("site","ID", "quad", "size21", "rep21", "sur_21_22","sur_21_22_tpry", "size22", "N22", "L22", "rep22", "Comm22")], rep(2021, dim(data)[1]))                                
+data22_23 <- cbind.data.frame(data[,c("site","ID", "quad", "size22", "rep22", "sur_22_23","sur_22_23_tpry","size23", "N23", "L23", "rep23", "Comm23")], rep(2022, dim(data)[1]))                                
+data23_24 <- cbind.data.frame(data[,c("site","ID", "quad", "size23", "rep23", "sur_23_24","sur_23_24_tpry","size24", "N24", "L24", "rep24", "Comm24")], rep(2023, dim(data)[1]))                                
+data24_25 <- cbind.data.frame(data[,c("site","ID", "quad", "size24", "rep24", "sur_24_25","sur_24_25_tpry","size25", "N25", "L25", "rep25", "Comm25")], rep(2024, dim(data)[1]))                                
 
 
-names(data15_16) <-  names(data16_17) <-  names(data17_18) <-  names(data18_19) <-  names(data19_20) <-  names(data20_21) <-  names(data21_22) <- names(data22_23) <- names(data23_24) <-
-  c("site", "ID", "quad", "size0", "rep0", "libsur0_1", "consur0_1","size1", "N1", "L1",  "rep1","comm1", "startyear")
-head(data23_24)
-data_long <- rbind(data15_16, data16_17, data17_18, data18_19, data19_20, data20_21, data21_22, data22_23, data23_24)   
+names(data15_16) <-  names(data16_17) <-  names(data17_18) <-  names(data18_19) <-  
+  names(data19_20) <-  names(data20_21) <-  names(data21_22) <- names(data22_23) <- 
+  names(data23_24) <- names(data24_25) <- c("site", "ID", "quad", "size0", "rep0", "sur0_1", "sur0_1_tp","size1", "N1", "L1",  "rep1","comm1", "startyear")
+head(data24_25)
+data_long <- rbind(data15_16, data16_17, data17_18, data18_19, data19_20, data20_21, data21_22, data22_23, data23_24, data24_25)   
       data_long$site <- factor(data_long$site)    
       data_long$quad <- factor(data_long$quad)   
       data_long$size0 <- as.numeric(data_long$size0) 
       data_long$rep0 <- as.numeric(data_long$rep0)  
       data_long$size1 <- as.numeric(data_long$size1)  
       data_long$rep1 <- as.numeric(data_long$rep1)  
-      data_long$ startyear <- factor(data_long$ startyear)
+      data_long$startyear <- factor(data_long$ startyear)
 
 # adding the "fine detailed" fire history from Natalie's FireHistory.rmd file---- 
       
 #load in fire file
-fire_histories <- read.csv("processed-data/fire_histories_8_27_2025.csv") ## load in the most recent fire history fire
+fire_histories <- read.csv("processed-data/fire_histories_landscape.csv") ## load in the most recent fire history fire
       
 data_long$TSF <- NA
 data_long$TBF <- NA
 #copy an paste TSF and TBF into data_long
 for (i in 1:nrow(data_long)) {
   #if (is.na(data_long$quad[i])) {  # Only proceed if quad is NA
-    match_TSF <- fire_histories$TSF[fire_histories$record_type== "manager"&
+    match_TSF <- fire_histories$TSF[
                                       fire_histories$site == data_long$site[i] & 
                                       fire_histories$startyear == data_long$startyear[i]]
     
@@ -93,7 +94,7 @@ for (i in 1:nrow(data_long)) {
 for (i in 1:nrow(data_long)) {
   #if (is.na(data_long$quad[i])) {  # Only proceed if quad is NA
   #match_TSF <- fire_histories$TSF[fire_histories$site == data_long$site[i] & fire_histories$startyear == data_long$startyear[i]]
-  match_TBF <- fire_histories$TBF[fire_histories$record_type== "manager"&fire_histories$site == data_long$site[i] & fire_histories$startyear == data_long$startyear[i]]
+  match_TBF <- fire_histories$TBF[fire_histories$site == data_long$site[i] & fire_histories$startyear == data_long$startyear[i]]
   
   if (length(match_TBF) == 1) {  # Ensure there's exactly one match
     data_long$TBF[i] <- match_TBF
@@ -104,4 +105,4 @@ for (i in 1:nrow(data_long)) {
 
 #################################################################################################
 
-write.csv(data_long, file= "data/TBFxClimate/TBF_long_lm.csv")
+write.csv(data_long, file= "data/TBFxClimate/TBF_long_landscape.csv")
